8cf8da6b64b18c6e16a5ae11ba8904ca5f317f9c
Yangkai <yangkai030208@163.com>
2017-04-19 20:18:37 +0800

    20170419

--- route/index.js
+++ route/index.js
@@ -5,10 +5,9 @@
 var express = require('express');
 var router = express.Router();
 var querystring = require('querystring');
-var user = require('../entity/user');
+var userEntity = require('../entity/user');
 var userServer = require('../server/userServer');
-var sha1 = require('utility');
-var superagent = require('superagent');
+
 module.exports = function(app){
     app.get(
         '/getlogin',
@@ -25,12 +24,11 @@ module.exports = function(app){
    app.get(
        '/login',
        function(req,res){
+
            var data = req.query;
-           console.log(data.username);
-           console.log(data.password);
-           var url = data.url-0;
            userServer.loginUserServer(data,function(results){
                if(results.length>=1){
+                   var user = new userEntity();
                    user.userName = results[0].userName;
                    user.userAddress = results[0].userAddress;
                    user.userEmail = results[0].userEmail;
@@ -42,13 +40,7 @@ module.exports = function(app){
                    user.userType = results[0].userType;
                    user.userBack =  results[0].userBack;
                    req.session.user = user;
-                   var str = 'logint_state=1&userName='+user.userName+'&userAddress='+user.userAddress+
-                             "&userEmail="+user.userEmail+"&userId="+user.userId+
-                             "&userIntroduce="+user.userIntroduce+"&userSex="+user.userSex+
-                             "&userBack="+user.userBack+"&userState="+user.userState+
-                             "&userHead="+user.userHead+"&userType="+user.userType;
-                   //如果登陆成功.
-                   res.send({logint_state:1,user:str});
+                   res.send({logint_state:1,user:JSON.stringify(user) });
                }else{
                    res.send({logint_stat:0});
                }
